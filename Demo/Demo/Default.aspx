47eb08dac42a321026f4243e6c7106cc10d4092d
Sandeep N <35574314+sndeep73@users.noreply.github.com>
2023-06-14 09:37:00 +0530

    adding demo

---
+++ Demo/Demo/Default.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Demo._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+          <br />
+       <asp:Label Text="Enter Department to add" runat="server" /><asp:TextBox ID="DeptTB" runat="server" />
+       <asp:Button ID="Button2" runat="server" Text="Add Dept" OnClick="Button2_Click" />
+        
+
+   
+
+</asp:Content>
---
+++ Demo/Demo/Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Demo
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // string connectionstring = ConfigurationManager.ConnectionStrings["MyConnStr"].ConnectionString;
+            string connectionstring = ConfigurationManager.ConnectionStrings["DemoConnStr"].ConnectionString;
+            using (SqlConnection conn= new SqlConnection(connectionstring))
+            {
+                using (SqlCommand command= new SqlCommand("Insert into [Customers](Name,IsSubsribedToNewsLetter,Membershiptype_Id) values('San',1,1)", conn))
+                {
+                    conn.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+           string dept= DeptTB.Text;
+            string connectionstring = ConfigurationManager.ConnectionStrings["DemoConnStr"].ConnectionString;
+            string query = "Insert into Department(DeptId,DeptName) Values(2,'" + dept + "')" ;
+            using (SqlConnection conn = new SqlConnection(connectionstring))
+            {
+                using(SqlCommand cmd= new SqlCommand(query,conn))
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
